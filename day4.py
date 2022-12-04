e4befd2ffa701062c01af6aa3c3bab301d2f3dbe
lelilia <lelilia@posteo.de>
2022-12-04 06:14:39 +0100

    day 4

--- day4.py
+++ day4.py
@@ -6,13 +6,13 @@ Advent of Code 2022
 with open("input4", "r") as f:
     input = f.readlines()
 
-count_1= 0
+count_1 = 0
 count_2 = 0
 
 for line in input:
     a_1, a_2, b_1, b_2 = [int(x) for y in line.split(",") for x in y.split("-")]
 
-    if a_1<= b_1 and a_2 >= b_2 or b_1 <= a_1 and b_2 >= a_2:
+    if a_1 <= b_1 and a_2 >= b_2 or b_1 <= a_1 and b_2 >= a_2:
         count_1 += 1
     if a_1 <= b_1 <= a_2 or b_1 <= a_1 <= b_2:
         count_2 += 1
