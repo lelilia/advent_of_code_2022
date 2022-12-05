b2a15f27081f1d833af41a0d466b3b4740511269
lelilia <lelilia@posteo.de>
2022-12-05 08:52:20 +0100

    day 5

---
+++ day5.py
@@ -0,0 +1,38 @@
+"""
+Advet of Code 2022
+--- Day 5: Supply Stacks ---
+"""
+
+with open("input5", "r") as f:
+    input = f.read()
+
+stack, commands = input.split("\n\n")
+stacks = {}
+
+for line in stack.split("\n")[-2::-1]:
+    for i, element in enumerate(line[1:-1:4]):
+        if element == " ":
+            continue
+        if i + 1 not in stacks:
+            stacks[i + 1] = []
+        stacks[i + 1].append(element)
+
+
+stacks_1 = stacks.copy()
+stacks_2 = stacks.copy()
+
+
+for line in commands.split("\n"):
+    _, amount, _, origin, _, destination = line.strip().split(" ")
+    amount, origin, destination = int(amount), int(origin), int(destination)
+    stacks_2[origin], stacks_2[destination] = (
+        stacks_2[origin][:-amount],
+        stacks_2[destination] + stacks_2[origin][-amount:],
+    )
+    while amount > 0:
+        curr = stacks_1[origin].pop()
+        stacks_1[destination].append(curr)
+        amount -= 1
+
+print("Part 1:", "".join([x[-1] for x in stacks_1.values()]))
+print("Part 2:", "".join([x[-1] for x in stacks_2.values()]))
